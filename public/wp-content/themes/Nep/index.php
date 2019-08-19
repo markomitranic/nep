689f622e42494ffc14633523e967d0bd50d6d36b
Marko Mitranic <marko.mitranic@catenamedia.com>
2019-08-19 23:19:06 +0200

    Finish news and news search pages

--- public/wp-content/themes/Nep/index.php
+++ public/wp-content/themes/Nep/index.php
@@ -1,84 +1,110 @@
 <?php get_header(); ?>
-<?php get_template_part('partials/masthead'); ?>
+
+<div id="masthead">
+    <div class="wrapper">
+        <div id="logo">
+            <a href="<?php echo home_url(); ?>" title="Go to the front page">
+				<?=file_get_contents(get_template_directory() . '/assets/global/NEP Logo.svg')?>
+            </a>
+        </div>
+        <div id="page-title">
+            <h1>Vesti</h1>
+            <div id="years">
+                <ul>
+                    <li><a href="#">2019</a></li>
+                    <li><a href="#">2018</a></li>
+                    <li><a href="#">2017</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div id="content">
 	<?php get_template_part('partials/sidebar'); ?>
 	<main>
 
-		<div id="intro">
-			<p>Jednogodišnji interdisciplinarni eksperimentalni obrazovni program za studente različitih fakulteta utemeljen na principima saradnje, timskog rada, dijaloga, razmene, otvorenosti i povezivanja različitih znanja, veština i kapaciteta.</p>
-		</div>
+        <div id="search">
+            <div class="wrapper">
+                <form  role="search" method="get" id="searchform" class="searchform" action="#">
+                    <label class="screen-reader-text" for="s">Pretraga za:</label>
+                    <?php
+                        $searchQuery = '';
+                        if (array_key_exists('s', $_REQUEST)) {
+                            $searchQuery = $_REQUEST['s'];
+                        }
+                    ?>
+                    <input type="text" value="<?=$searchQuery?>" placeholder="Pretraga" name="s" id="s">
+                    <button>&nbsp;</button>
+                </form>
+            </div>
+        </div>
 
-		<div id="funnel-blocks">
-			<div id="news">
-				<ul>
-					<li>
-						<a href="#" title="">
-							<div class="image" style="background-image:url('<?=get_template_directory_uri()?>/assets/temp/vesti-naslovna.png');">
-								<img src="<?=get_template_directory_uri()?>/assets/temp/vesti-naslovna.png" alt="">
-							</div>
-							<div class="info">
-								<h2>Vesti</h2>
-								<p class="counter">1/6</p>
-								<p class="excerpt shouldShave" data-rows="3">Predavanje Marije Kojić, učesnice 2. genereracije NEP-a, na Beogradskom Sajmu Nameštaja</p>
-							</div>
-						</a>
-					</li>
-					<li>
-						<a href="#" title="">
-							<div class="image" style="background-image:url('<?=get_template_directory_uri()?>/assets/temp/media-naslovna.jpg');">
-								<img src="<?=get_template_directory_uri()?>/assets/temp/media-naslovna.jpg" alt="">
-							</div>
-							<div class="info">
-								<h2>Vesti</h2>
-								<p class="counter">2/6</p>
-								<p class="excerpt shouldShave" data-rows="3">Marko Mitranic je odradio neverovatan podvig Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores delectus dolorem fugit laborum nam placeat quis quo sed sequi. Animi eius, eligendi id ipsum nulla officiis pariatur tenetur totam unde!</p>
-							</div>
-						</a>
-					</li>
-				</ul>
-			</div>
-			<div id="programs">
-				<a href="#" title="">
-					<h2>Program</h2>
-					<p>radionica / predavanje prezentacija / poseta / mentorska sesija / finalni događaj / ekskurzija</p>
-				</a>
-			</div>
-			<div id="media">
-				<a href="#" title="">
-					<div class="image" style="background-image:url('<?=get_template_directory_uri()?>/assets/temp/media-naslovna.jpg');">
-						<img src="<?=get_template_directory_uri()?>/assets/temp/media-naslovna.jpg" alt="">
-					</div>
-					<div class="info">
-						<h2>Media</h2>
-					</div>
-				</a>
-			</div>
-			<div id="empty" style="background-image:url('<?=get_template_directory_uri()?>/assets/empties/Asset <?=rand(1,9)?>.svg');"></div>
-			<div id="event" class="full-block">
-				<a href="" title="">
-					<div class="image" style="background-image:url('<?=get_template_directory_uri()?>/assets/temp/izlozba-naslovna.jpg');">
-						<img src="<?=get_template_directory_uri()?>/assets/temp/izlozba-naslovna.jpg" alt="">
-					</div>
-					<div class="info">
-						<div class="announcement">
-							<p class="program">#nep4</p>
-							<h2>Final Exhibition</h2>
-						</div>
-						<div class="signature">
-							<p class="venue">G12HUB</p>
-							<?php
-							$startDate = new DateTimeImmutable();
-							$endDate = new DateTimeImmutable('+ 25 days');
-							?>
-							<p class="date"><?=$startDate->format('d. M')?> - <?=$endDate->format('d. M')?></p>
-						</div>
-					</div>
-				</a>
-			</div>
-		</div>
+        <div id="news-list">
+            <?php if (have_posts()) : ?>
+            <ul>
+	            <?php
+                    while (have_posts()) :
+	                    the_post();
+                ?>
+                <li>
+                    <a href="#" title="">
+                        <div class="image" style="background-image:url('<?=get_template_directory_uri()?>/assets/temp/vesti-naslovna.png');">
+                            <img src="<?=get_template_directory_uri()?>/assets/temp/vesti-naslovna.png" alt="">
+                        </div>
+                        <div class="info">
+                            <h2><?=get_the_title()?></h2>
+                            <p class="excerpt shouldShave" data-rows="3"><?=getExcerpt(get_the_excerpt(), 20)?></p>
+                        </div>
+                    </a>
+                </li>
+                <?php endwhile; ?>
+            </ul>
+            <?php else : ?>
+                <div id="no-results">
+                    <p>Na žalost nema pronađenih vesti za termin: "<?=$searchQuery?>".</p>
+                    <p>Ispod se nalaze neke od najnovijih vesti, možda je to ono što tražite?</p>
+                </div>
 
-	</main>
+                <ul>
+	                <?php
+                        $latestPostsQuery = new WP_Query([
+                            'posts_per_page' => 4
+                        ]);
+
+                        if ( $latestPostsQuery->have_posts() ) :
+                            while ( $latestPostsQuery->have_posts() ) :
+                                $latestPostsQuery->the_post();
+                    ?>
+                    <li>
+                        <a href="#" title="">
+                            <div class="image" style="background-image:url('<?=get_template_directory_uri()?>/assets/temp/vesti-naslovna.png');">
+                                <img src="<?=get_template_directory_uri()?>/assets/temp/vesti-naslovna.png" alt="">
+                            </div>
+                            <div class="info">
+                                <h2><?=get_the_title()?></h2>
+                                <p class="excerpt shouldShave" data-rows="3"><?=getExcerpt(get_the_excerpt(), 20)?></p>
+                            </div>
+                        </a>
+                    </li>
+                    <?php
+                            endwhile;
+                        endif;
+                        wp_reset_postdata();
+	                ?>
+                </ul>
+            <?php endif; ?>
+        </div>
+
+		<?php
+            the_posts_pagination([
+                'mid_size'  => 2,
+                'prev_text' => '<span class="nav-prev-text">Newer posts</span>',
+                'next_text' => '<span class="nav-prev-text">Older posts</span>'
+            ]);
+        ?>
+
+    </main>
 </div>
 
 <?php get_footer(); ?>
